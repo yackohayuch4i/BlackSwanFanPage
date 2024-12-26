126aadd4fbc28ef8ed0cd773a8f9f7a87bf12a3a
yackohayuch4i <korobkatoect@gmail.com>
2024-12-26 18:22:27 +0200

    code

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('redirectButton').addEventListener('click', function() {
+    window.location.href = 'https://honkai-star-rail.fandom.com/wiki/Black_Swan';
+});
+
+document.getElementById('ultimate').addEventListener('click' , function() {
+    window.location.href = 'ultimate.html'
+});
